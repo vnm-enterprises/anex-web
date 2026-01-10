1d2431b86eeedcf2603c06c2b858bd840d7ec554
vihanga <vihanganethusara00@gmail.com>
2026-01-10 16:54:08 +0530

    Add/update app/auth/signup/page.tsx

---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,12 @@
+import RegisterImagePanel from "@/components/auth/RegisterImagePanel";
+import RegisterForm from "@/components/auth/RegisterForm";
+
+export default function RegisterPage() {
+  return (
+
+    <main className="h-screen w-full overflow-hidden flex bg-background-light dark:bg-background-dark">
+          <RegisterImagePanel />
+        <RegisterForm />
+        </main>
+  );
+}
