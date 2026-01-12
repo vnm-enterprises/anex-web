3b338f9076dd08474f4e185132137c3c477aa9ec
vihanga <vihanganethusara00@gmail.com>
2026-01-12 16:36:30 +0530

    Add/update middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("auth-storage")?.value;
+
+  const protectedRoutes = ["/dashboard", "/list", "/settings"];
+
+  if (
+    protectedRoutes.some((path) =>
+      req.nextUrl.pathname.startsWith(path)
+    ) &&
+    !token
+  ) {
+    return NextResponse.redirect(
+      new URL("/auth/login", req.url)
+    );
+  }
+
+  return NextResponse.next();
+}
