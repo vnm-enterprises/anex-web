68e36820a2f11b088399a0cc66c91663cddc138b
vihanga <vihanganethusara00@gmail.com>
2026-01-13 20:41:04 +0530

    feat: removal of react query

--- queries/property.queries.ts
+++
@@ -1,7 +0,0 @@
-import api from "@/lib/api";
-import { Property } from "@/types/property";
-
-export const fetchMyProperties = async (): Promise<Property[]> => {
-  const res = await api.get("/properties/my");
-  return res.data;
-};
