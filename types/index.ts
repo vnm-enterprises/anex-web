a28f2c45fdf7842a188522c8bfee99ab14c514f1
vihanga <vihanganethusara00@gmail.com>
2026-01-31 14:33:16 +0530

    Add/update types/index.ts

--- types/index.ts
+++ types/index.ts
@@ -2,6 +2,6 @@ export * from "./common";
 export * from "./auth";
 export * from "./user";
 export * from "./landlord";
-export * from "./property";
+export * from "./propertys";
 export * from "./rating";
 export * from "./saved-listing";
