4ef3402a187293f3ccbf22f37449cd29b5b072e4
vihanga <vihanganethusara00@gmail.com>
2026-01-31 14:33:16 +0530

    Add/update components/dashboard/ListingsSection.tsx

--- components/dashboard/ListingsSection.tsx
+++ components/dashboard/ListingsSection.tsx
@@ -28,7 +28,10 @@ export interface Listing {
   description: string;
   views: number;
   status: ListingStatus;
-  image: string;
+  propertyImages: string;
+  bedrooms?: number
+  bathrooms? : number;
+  sizeSqft?: any;
 }
 
 /* -------------------- */
@@ -44,7 +47,7 @@ const DUMMY_LISTINGS: Listing[] = [
       "Spacious upstairs annex with separate entrance, fully tiled, parking available. Walking distance to High Level road.",
     views: 1240,
     status: "active",
-    image:
+    propertyImages:
       "https://lh3.googleusercontent.com/aida-public/AB6AXuDmw2mBDhmH7tct4JnO4mZVRpA8TGfTs5W-k-MMYy1T-r27Itr6I7_HnAv_HZe52GXgOkODJ-8PJXfR3BtmrXZ7NlyMGNwLNfIJmnifTt82wpHK5gYTE9e1G2-P8-ttwlHux14M7uwySkPf_Ma554qw4jUVpLYGQ1YwgPtramvB9Nz0hH-1ZCmgwLxzkSMxF-9Qq0zL-JHQlfQeCyzKBGPvCaI3Y4ge23zSXIG2oWtuitU7XtawNejSAXICtcG1smHHy6SVjqi7uX8",
   },
   {
@@ -56,7 +59,7 @@ const DUMMY_LISTINGS: Listing[] = [
       "Ideal for university students. Shared bathroom, cooking facility available. Close to Peradeniya University.",
     views: 450,
     status: "reviewing",
-    image:
+    propertyImages:
       "https://lh3.googleusercontent.com/aida-public/AB6AXuDmw2mBDhmH7tct4JnO4mZVRpA8TGfTs5W-k-MMYy1T-r27Itr6I7_HnAv_HZe52GXgOkODJ-8PJXfR3BtmrXZ7NlyMGNwLNfIJmnifTt82wpHK5gYTE9e1G2-P8-ttwlHux14M7uwySkPf_Ma554qw4jUVpLYGQ1YwgPtramvB9Nz0hH-1ZCmgwLxzkSMxF-9Qq0zL-JHQlfQeCyzKBGPvCaI3Y4ge23zSXIG2oWtuitU7XtawNejSAXICtcG1smHHy6SVjqi7uX8",
   },
   {
@@ -68,7 +71,7 @@ const DUMMY_LISTINGS: Listing[] = [
       "Currently rented out until Dec 2024. 4 Bedrooms, 3 Bathrooms, Servant quarters, Solar power.",
     views: 2100,
     status: "occupied",
-    image:
+    propertyImages:
       "https://lh3.googleusercontent.com/aida-public/AB6AXuDmw2mBDhmH7tct4JnO4mZVRpA8TGfTs5W-k-MMYy1T-r27Itr6I7_HnAv_HZe52GXgOkODJ-8PJXfR3BtmrXZ7NlyMGNwLNfIJmnifTt82wpHK5gYTE9e1G2-P8-ttwlHux14M7uwySkPf_Ma554qw4jUVpLYGQ1YwgPtramvB9Nz0hH-1ZCmgwLxzkSMxF-9Qq0zL-JHQlfQeCyzKBGPvCaI3Y4ge23zSXIG2oWtuitU7XtawNejSAXICtcG1smHHy6SVjqi7uX8",
   },
 ];
@@ -94,8 +97,9 @@ export default function ListingsSection() {
    */
   const fetchListings = async () => {
     try {
-      const res = await api.get("/listings");
-      setListings(res.data);
+      const res = await api.get("/properties");
+      console.info(res.data);
+      setListings(res.data.properties || []);
     } catch (err: any) {
       setError("Failed to load listings. Please try again later.");
       console.error("Listings fetch error:", err);
@@ -105,18 +109,19 @@ export default function ListingsSection() {
   };
 
   useEffect(() => {
-    const isDevMode = "false";
+    const isDevMode = true;
+    fetchListings();
 
-    if (isDevMode) {
-      // Use dummy data in development
-      setTimeout(() => {
-        setListings(DUMMY_LISTINGS);
-        setLoading(false);
-      }, 300);
-    } else {
-      // Fetch real data in production
-      fetchListings();
-    }
+    // if (isDevMode) {
+    //   // Use dummy data in development
+    //   setTimeout(() => {
+    //     setListings(DUMMY_LISTINGS);
+    //     setLoading(false);
+    //   }, 300);
+    // } else {
+    //   // Fetch real data in production
+    //   fetchListings();
+    // }
   }, []);
 
   if (loading) {
