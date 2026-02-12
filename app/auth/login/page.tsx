d462c69253448910448bd2dc6e3b308a431dc5f6
vihanga <vihanganethusara00@gmail.com>
2026-02-12 22:25:58 +0530

    bugfix: build error 004

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,7 @@
 
 import AuthImagePanel from "@/components/auth/AuthImagePanel";
 import LoginForm from "@/components/auth/LoginForm";
+import { Suspense } from "react";
 
 export default function LoginPage() {
 
@@ -8,7 +9,9 @@ export default function LoginPage() {
   return (
     <main className="h-screen w-full overflow-hidden flex bg-background-light dark:bg-background-dark">
       <AuthImagePanel />
+       <Suspense fallback={<div>Loading...</div>}>
       <LoginForm />
+    </Suspense>
     </main>
   );
 }
