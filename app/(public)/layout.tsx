6eefb9129498f85dc6aa88d6f03095323e97b5af
vihanga <vihanganethusara00@gmail.com>
2026-02-15 14:15:15 +0530

    feat: layout cnages

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -10,8 +10,8 @@ export default function PublicLayout({
   return (
     <div className="flex min-h-screen flex-col">
       <SiteHeader />
-      <main className="flex-1">
-       
+      <main className="flex-1 min-h-screen">
+
         {children}</main>
       <SiteFooter />
     </div>
