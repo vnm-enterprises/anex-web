a5eb7ed755ce288b6edaccb1810178a7ed244688
vihanga <vihanganethusara00@gmail.com>
2026-03-17 10:58:08 +0530

    feat: add useDebounce hook for value debouncing functionality

---
+++ hooks/use-debounce.ts
@@ -0,0 +1,22 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+/**
+ * Returns a debounced copy of a value after the provided delay.
+ */
+export function useDebounce<T>(value: T, delay = 400): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = window.setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      window.clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
